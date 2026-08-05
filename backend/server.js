e6ba53a7b41d3dfa29b2684435070278cb2b557b
sheykhizadeh <morteza.sheykhizadeh@gmail.com>
2026-08-05 19:58:44 +0200

    feat(server): configure Cloudinary during startup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import app from './app.js'
+import configureCloudinary from './config/cloudinary.js'
 import connectDB from './config/mongodb.js'
 
 const PORT = process.env.PORT || 4000
 
 try {
+  configureCloudinary()
   await connectDB()
 
   app.listen(PORT, () => {
