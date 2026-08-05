c79ebff3072f2b5347883294cc7c5ec027b9323e
sheykhizadeh <morteza.sheykhizadeh@gmail.com>
2026-08-05 18:51:09 +0200

    feat(database): add MongoDB connection adapter

---
+++ backend/config/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI
+
+  if (!uri) {
+    throw new Error('MONGODB_URI is not defined')
+  }
+
+  await mongoose.connect(uri, {
+    dbName: 'hotel'
+  })
+
+  console.log('MongoDB connection established')
+}
+
+export default connectDB
