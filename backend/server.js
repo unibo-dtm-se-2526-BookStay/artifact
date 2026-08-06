8fb696c6c5dae700b9d436c53e8e4e8751f2bbae
sheykhizadeh <morteza.sheykhizadeh@gmail.com>
2026-08-06 21:12:56 +0200

    refactor(server): wrap startup in async function

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 import 'dotenv/config'
 import app from './app.js'
-import configureCloudinary from './config/cloudinary.js'
 import connectDB from './config/mongodb.js'
+import configureCloudinary from './config/cloudinary.js'
 
 const PORT = process.env.PORT || 4000
 
-try {
-  configureCloudinary()
-  await connectDB()
+const startServer = async () => {
+  try {
+    configureCloudinary()
+    await connectDB()
 
-  app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`)
-  })
-} catch (error) {
-  console.error(`Server startup failed: ${error.message}`)
-  process.exit(1)
+    app.listen(PORT, () => {
+      console.log(`Server started on Port: ${PORT}`)
+    })
+  } catch (error) {
+    console.error(`Server startup failed: ${error.message}`)
+    process.exit(1)
+  }
 }
+
+startServer()
